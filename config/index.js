db99de28a1fd1ed3f286b23b8bb607c07f8bfb79
Gary-A1 <62493559+Gary-A1@users.noreply.github.com>
2022-08-27 03:06:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,7 +16,7 @@ export const config = {
     city: "赣州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "2001", "date": "06-05", "type": 'r'},
+      {"name": "老婆", "year": "2001", "date": "06-05", "type": 'r'}
     ],
     // 在一起的日子，格式同上
     loveDate: "2020-02-12"
